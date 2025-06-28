bc86e17b14b92cf0ac98390212fb66d5871ec496
-PC\Omistaja <niko.sukari@hotmail.com>
2025-06-28 15:49:29 +0300

    puhelinluettelo valmis

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,68 +1,157 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import FilterForm from './components/filter_form'
 import PersonForm from './components/person_form'
 import Filter from './components/filter'
+import DelButton from './components/del_button'
+import personService from './services/persons'
+
+const AddNotification = ({message, setAddMsg}) => {
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAddMsg(null)
+    }, 4000)
+    return () => clearTimeout(timeOut)
+  }, [])
+  const styles = {
+    color: 'black',
+    fontStyle: 'italic',
+    backgroundColor: 'palegreen',
+    fontSize: 'large'
+  }
+  return (
+    <>
+    <div>
+      <p className='added' style={styles}>
+        {message}
+      </p>
+    </div>
+    </>
+  )
+}
+
+const ChangeNotification = ({message, setChangeMsg, setIsChanged, isChanged}) => {
+  useEffect(() => {
+    if (isChanged !== null) {
+      const timeout = setTimeout(() => {
+        setChangeMsg(null);
+        setIsChanged(null);
+      }, 6000);
+      return () => clearTimeout(timeout)
+    }
+  }, [isChanged, setChangeMsg, setIsChanged]);
+
+  if (isChanged === true) {
+    const styles = {
+      backgroundColor: 'lightgreen',
+      color: 'black',
+      fontStyle: 'italic',
+      fontSize: 'large'
+    }
+    return (
+      <div style={styles}>{message}</div>
+    )
+  } else if (isChanged === false) {
+    const styles = {
+      backgroundColor: 'indianred',
+      color: 'black',
+      fontStyle: 'italic',
+      fontSize: 'large'
+    }
+    return (
+      <div style={styles}>{message}</div>
+    );
+  } else {
+    console.log('something weird in ChangeNotification')
+    return null;
+  }
+}
+
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-123456',
-      id: 1
-    },
-    { name: 'Arska Hella',
-      number: '040-234567',
-      id: 2
-
-    }
-     
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [showAll, setShowed] = useState('')
   const [filteredName, setFilteredName] = useState('')
-  const [mappedPersons, setMappedPersons] = useState([])
+  const [addMsg, setAddMsg] = useState(null)
+  const [changeMsg, setChangeMsg] = useState(null)
+  const [isChanged, setIsChanged] = useState(null)
 
-  const handleName = (event) => {
-    setNewName(event.target.value)
-  }
 
-  const handlePhonenum = (event) => {
-    setNewNumber(event.target.value)
-  }
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addContact = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
-    } 
-    if (persons.some(person => person.name === personObject.name)) {
-      alert(`${newName} is already added to phonebook`)
-      return
-    } else {
-      return (
-        setPersons(persons.concat(personObject)),
-        setNewName(''),
-        setNewNumber(''),
-        console.log(persons)
-      )
+      id: `${persons.length + 1}`
     }
+    const personToCheck = persons.filter(person => person.name === personObject.name)
+    const personToPost = {...personToCheck[0], number: personObject.number}
+
+    
+    if (personToCheck.length > 0) {
+      if (window.confirm('Person already found. Do you want to change the phone number?')) {
+        personService
+          .update(personToCheck[0].id, personToPost)
+          .then(() => personService.getAll())
+          .then(response => {
+            setPersons(response.data)
+            setIsChanged(true)
+            setChangeMsg(`${personToPost.name}'s phone number change succeeded`)
+          })
+          .catch(error => {
+            console.log('error here', error);
+            setIsChanged(false)
+            setChangeMsg(`${personToPost.name} is deleted already`)
+          })
+        return
+      } else {
+        return
+      }
+    }
+    personService
+      .create(personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+        setAddMsg(`${personObject.name} added`)
+      })
   }
 
-  const handleFilter = (event) => {
-    setFilteredName(event.target.value)
-  }
 
-  return (
-    <div>
+const handleName = (event) => {
+  setNewName(event.target.value)
+}
+
+const handlePhonenum = (event) => {
+  setNewNumber(event.target.value)
+}
+
+
+const handleFilter = (event) => {
+  setFilteredName(event.target.value)
+}
+
+return (
+  <div>
       <h2>Phonebook</h2>
+      {changeMsg && <ChangeNotification message={changeMsg} setChangeMsg={setChangeMsg} setIsChanged={setIsChanged} isChanged={isChanged}/>}
       <FilterForm handler={handleFilter} filter={filteredName}/>
       <h2>Add new</h2>
+      {addMsg && <AddNotification message={addMsg} setAddMsg={setAddMsg} />}
       <PersonForm addFunc={addContact} nameHandler={handleName} numHandler={handlePhonenum} name={newName} number={newNumber}/>
       <h2>Numbers</h2>
-      <Filter persons={persons} filter={filteredName}/>
+      <Filter persons={persons} filter={filteredName} setPersons={setPersons}/>
     </div>
   )
 }
--- part2/puhelinluettelo/src/components/filter.jsx
+++ part2/puhelinluettelo/src/components/filter.jsx
@@ -1,5 +1,6 @@
+import DelButton from "./del_button"
 
-const Filter = ({persons, filter}) => {
+const Filter = ({persons, filter, setPersons}) => {
   const filteredPersons = persons.filter(person => person.name.toLowerCase().startsWith(filter.toLowerCase()))
   if (filteredPersons.length === 0) {
     return (
@@ -9,13 +10,13 @@ const Filter = ({persons, filter}) => {
   } else if (filter.length === 0) {
     return (
         persons.map(person =>
-            <li key={person.id}>{person.name} {person.number}</li>
+            <li key={person.id}>{person.name} {person.number} <DelButton id={`${person.id}`} setPersons={setPersons} persons={persons}/></li>
           )
     )
   } else {
     return (
       filteredPersons.map(person =>
-        <li key={person.id}>{person.name} {person.number}</li>
+        <li key={person.id}>{person.name} {person.number} <DelButton id={`${person.id}`} setPersons={setPersons} persons={persons}/></li>
       )
     )
   }
